69f15278d35200f306f6e2ec347aa90f30e324d0
soso1996 <soso.arxoshashvili@gmail.com>
2022-12-28 16:18:55 +0400

    homework1

---
+++ homework.js
@@ -0,0 +1,40 @@
+/////task1///
+
+let sisterName = 'ana';
+let sisterAge = 28;
+let bratherName = 'levani';
+let bratherAge= sisterAge -7 ;
+let ageDiference = "ana levanze 7 wliT ufrosia";
+
+console.log(bratherAge)
+
+
+let studentsName = ['John','Sam','Ann','Will','Andrew','Joseph','Lorelai','Amelie','Den'];
+
+let middleEl= studentsName[studentsName.length/2 - 0.5];
+console.log(middleEl);
+
+const student = {
+    firstName: 'soso',
+    lastName: 'arkhoshashvili',
+    age: 26,
+    subjects: {
+        humanitarian: ['history', 'English', 'Geography','politology','psichology'],
+        technical:['math','physics','economic','finance','informatic'],
+    },
+    roommate: {
+       fullName: 'David kapanadze',
+        age: '28',
+    }
+    
+}
+let fullName = student.firstName + " " + student.lastName
+
+let studentFullname= student[fullName];
+console.log(getPropertyval)
+
+for( i = o; i < student.subjects.humanitarian.length; i++){
+    console.log(student.subjects.humanitarian.length[i])
+}
+
+
